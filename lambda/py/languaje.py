c4212598b9fab96bc3081096e990e916108b6f47
joncolas <jcolas@audiense.com>
2023-04-08 13:16:53 +0200

    Fix some languaje string responses

--- lambda/py/languaje.py
+++ lambda/py/languaje.py
@@ -1,14 +1,14 @@
 strings = {
     "es-ES": {
-        "CHAT_GPT_ACTIVATED": "Chat GPT activado, ¿Qué necesitas?",
-        "ALEXA_TIMEOUT": "Lo siento, la pregunta que has hecho necesita más de 10 segundos para resolverse, lo cual está por encima de mis posibilidades, reformula la pregunta",
+        "CHAT_GPT_ACTIVATED": "¿Qué necesitas? empieza tu pregunta con las palabras 'dime' o 'busca'.",
+        "ALEXA_TIMEOUT": "Lo siento, la pregunta que has hecho necesita más de 10 segundos para resolverse, lo cual está por encima de mis posibilidades, reformula la pregunta.",
         "ASSISTANT_ROLE": "Eres un asistente amable y servicial.",
-        "CONTINUE": "¿Más preguntas?, recuerda que tienes que empezar la frase por dime o busca"
+        "CONTINUE": "¿Más preguntas?, recuerda empezar tu pregunta con las palabras 'dime' o 'busca'."
     },
     "en-US": {
-        "CHAT_GPT_ACTIVATED": "Chat GPT activated, What do you need?",
-        "ALEXA_TIMEOUT": "Sorry, your question require more than 10 seconds to resolve and that is out of my capabilities, repeat the question",
+        "CHAT_GPT_ACTIVATED": "What do you need? start your question with 'ask' or 'search' words.",
+        "ALEXA_TIMEOUT": "Sorry, your question require more than 10 seconds to resolve and that is out of my capabilities, repeat the question.",
         "ASSISTANT_ROLE": "You are a kind and helpful assistant.",
-        "CONTINUE": "More questions?, remember that you have to start your question with ask or search"
+        "CONTINUE": "More questions?, remember to start your question with 'ask' or 'search' words."
     }
 }
